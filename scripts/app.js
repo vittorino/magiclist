dc63754066b62d8cdcc0707f9bd6ff26615fd75f
Victor Stelmasuk <vittorino@gmail.com>
2021-01-14 13:50:59 -0300

    Version 0.2
    added card selection and collection from browser cookie

--- scripts/app.js
+++ scripts/app.js
@@ -1,23 +1,64 @@
 // get list of cards
 const getCardList = async () => {
-  const response = await fetch('json/4edcards_1.json');
+  const response = await fetch('json/4ed.json');
   const data = await response.json();
 
   return (data.cards);
 };
 
-// get reference container
+// get local collection
+const collection = localStorage.getItem('collection').split(',');
+
+// get gallery container and generate template
 const gallery = document.querySelector('.gallery');
 
-let html = ``;
+const generateTemplate = (card, owned) => {
+  const html = `
+    <div id="${card.multiverseid}" class="card ${card.colors[0].toLowerCase()} ${card.rarity.toLowerCase()} ${owned} bg-light shadow rounded">
+    <img src="img/cards/${card.name}.jpg" title="${card.name} (${card.rarity})" width="223px" />
+    </div>
+  `;
 
+  gallery.innerHTML += html;
+}
+
+let owned = "";
 getCardList().then(data => {
   data.forEach(function(card){
-    // create HTML template
-    html += `
-    <div class="card bg-light shadow rounded m-2">
-      <img src="img/cards/${card.name}.jpg" width="223px"/>
-    </div>`;
+    // create HTML gallery
+    if(collection.includes(card.multiverseid.toString())){
+      owned = "owned";
+    } else {
+      owned = "";
+    };
+    generateTemplate(card, owned);
   });
-  gallery.innerHTML = html;
+  
+  // click to mark owned cards
+  const boxes = document.querySelectorAll('.card');
+  boxes.forEach(box => {
+    box.addEventListener('click', () => {
+      if(!box.classList.contains('owned')){
+        box.classList.toggle('owned');
+        collection.push(box.id);
+        localStorage.setItem('collection', collection.toString());
+      } else {
+        box.classList.toggle('owned');
+        collection.splice(box.id);
+      }
+    });
+  });
+});
+
+const filters = document.querySelector('.btn-toolbar');
+filters.addEventListener('click', e => {
+    const color = e.target.classList[2].toLowerCase();
+    e.target.previousSibling.previousSibling.toggleAttribute('checked');
+    console.log(color);
+
+    let filtered = gallery.querySelectorAll('.'+color);
+    filtered.forEach(selected => {
+      selected.classList.toggle('hidden');
+    })
+    console.log(filtered);
 });
